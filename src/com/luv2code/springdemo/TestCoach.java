e004100eba4da78c7489eea7a67d05ccb09b29e1
MaT <mromatet@gmail.com>
2022-04-12 14:58:03 +0200

    creation Git repository

---
+++ src/com/luv2code/springdemo/TestCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springdemo;
+
+public class TestCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "it's my test Coach";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
