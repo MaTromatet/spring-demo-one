e5e4001028b66cdba9bc5906d279fe927765a4cd
MaT <mromatet@gmail.com>
2022-04-12 15:40:57 +0200

    Destroy Lifecycle and Prototype Scope

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -3,9 +3,12 @@ package com.luv2code.springdemo;
 public class TrackCoach implements Coach {
 
 	private FortuneService fortuneService;
+
+	public TrackCoach() {
+		
+	}
 	
 	public TrackCoach(FortuneService fortuneService) {
-		super();
 		this.fortuneService = fortuneService;
 	}
 
@@ -16,8 +19,26 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		
-		return "Just Do It: "+fortuneService.getFortune();
+		return "Just Do It: " + fortuneService.getFortune();
 	}
 
+	// add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");		
+	}
 }
+
+
+
+
+
+
+
+
+
+
